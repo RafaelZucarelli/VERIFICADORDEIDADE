ca5f5dd68850fbd0a740369f10e3689f5d56296f
RafaelZucarelli <zucarelli83@gmail.com>
2024-01-03 15:33:13 -0300

    Atualizado

--- script.js
+++ script.js
@@ -9,12 +9,41 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ' '
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked ){
             gênero = 'Homem'
+            if (idade >= 0 && idade <10 ){
+                //CRIANÇA
+                img.setAttribute('src', 'babyh.png')
+            } else if (idade >= 10 && idade < 21){
+                // Jovem HOMEM
+                img.setAttribute('src', 'jovemh.png')
+            } else if ( idade < 50) {
+                //Adulto HOMEM
+                img.setAttribute('src', 'homem.png')
+            } else {
+                // IDOSO
+                img.setAttribute('src', 'idoso.png')
+            }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if (idade >= 0 && idade <10 ){
+                //CRIANÇA
+                img.setAttribute('src', 'babym.png')
+            } else if (idade >= 10 && idade < 21){
+                // Jovem MULHER
+                img.setAttribute('src', 'jovemm.png')
+            } else if ( idade < 50) {
+                //Adulto MULHER
+                img.setAttribute('src', 'mulher.png')
+            } else {
+                // IDOSA 
+                img.setAttribute('src', 'idosa.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos um(a) ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
